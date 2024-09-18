6a5eb5ab93a3565426317e378c065b5dc1fdb413
PKpattranit999719 <103948605+PKpattranit999719@users.noreply.github.com>
2024-09-18 11:34:03 +0700

    เพิ่มตัวเลือกชุดข้อสอบกับทำปุ่มเลือกชั้นปี

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import HomeForm from './Components/Home/HomeForm';
 import LoginForm from './Components/LoginForm/LoginForm';
 import RegisterForm from './Components/Register/RegisterForm';
+import Lesson from './Components/Lesson/Lesson';
 function App() {
   return (
     <div>
       {/* <LoginForm /> */}
       {/* <RegisterForm/> */}
-      <HomeForm />
+      {/* <HomeForm /> */}
+      <Lesson />
     </div>
   );
 }
--- front_end/src/Components/Lesson/Lesson.js
+++ front_end/src/Components/Lesson/Lesson.js
@@ -22,19 +22,19 @@ const Lesson = () => {
                         Excepturi exercitationem possimus asperiores.`
         },
         {
-            title: 'lesson-4',
+            title: 'Lesson-4',
             text: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Molestias nemo iste eos,consequuntur pariatur quisquam ipsam?
                         Excepturi exercitationem possimus asperiores.`
         },
         {
-            title: 'lesson-5',
+            title: 'Lesson-5',
             text: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Molestias nemo iste eos,consequuntur pariatur quisquam ipsam?
                         Excepturi exercitationem possimus asperiores.`
         },
         {
-            title: 'lesson-6',
+            title: 'Lesson-6',
             text: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Molestias nemo iste eos,consequuntur pariatur quisquam ipsam?
                         Excepturi exercitationem possimus asperiores.`
@@ -44,6 +44,23 @@ const Lesson = () => {
     return (
         <div className='container'>
             <h1>Lesson</h1>
+            <div className='button-group'>
+                <button className='bth'>ประถมศึกษาปีที่ 1</button>
+                <button className='bth'>ประถมศึกษาปีที่ 2</button>
+                <button className='bth'>ประถมศึกษาปีที่ 3</button>
+            </div>
+
+            {/* Dropdown สำหรับชุดของข้อสอบ */}
+            <div className="dropdown-group">
+                <label htmlFor="exam-set">เลือกชุดของข้อสอบ:</label>
+                <select id="exam-set" className="dropdown">
+                    <option value="ชุดที่ 1">ชุดที่ 1</option>
+                    <option value="ชุดที่ 2">ชุดที่ 2</option>
+                    <option value="ชุดที่ 3">ชุดที่ 3</option>
+                    <option value="ชุดที่คุณสร้าง">ชุดที่คุณสร้าง</option>
+                </select>
+            </div>
+
             <div className='lessons'>
                 {lessons.map((lesson, index) => (
                     <div key={index} className='lesson'>
