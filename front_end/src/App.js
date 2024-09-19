ca23c08a6310e47fd896590a241b077704d2dc60
PKpattranit999719 <103948605+PKpattranit999719@users.noreply.github.com>
2024-09-19 23:41:56 +0700

    แก้ชื่อ folder

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
-import router from '../src/Components/Share/Routes'; 
+import router from '../src/Components/Shared/Routes'; 
 
 const App = () => {
   return (
